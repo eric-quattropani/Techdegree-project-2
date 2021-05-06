59b0c48933223381c4608738527d58d4604c8aeb
eric-quattropani <eric.quattropani@gmail.com>
2021-05-07 09:01:01 +1000

    Final commit
    My completed work!

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Techdegree-project-2
  Data Pagination and Filtering 
 
+This is my attempt of Project 2 going for a meets expectations grade. Although I understood most, if not all, of the concepts found in the instructions - I did find it a little difficult to start this one. 
 
-
-Notes for readMe and processes
-- Mention the decision over large pictures (originally tried the thumbnail but the quality was poor)
+The walkthrough was really helpful as it really simplified the thinking behind everything. It took me some time, but I learned a lot from this project!
--- js/script.js
+++ js/script.js
@@ -4,30 +4,17 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Created the showPage function which displays the student data. I was originally using the thumbnail object in the image source, but due to its poor quality ended up using the large object instead.
+Also I only realised at the end I could of just have assigned a constant to the 9 items. It would only help if I'd have to show more items per page for whatever reason.
 */
 function showPage(list, page) {
 
-   // create two variables which will represent the index for the first and last student on the page
   const startIndex = (page * 9) - 9;
   const endIndex = page * 9;
-   // select the element with a class of `student-list` and assign it to a variable
   const studentList = document.querySelector('.student-list');
-   // set the innerHTML property of the variable you just created to an empty string
-   studentList.innerHTML = '';
+  studentList.innerHTML = '';
 
-   // loop over the length of the `list` parameter
   for (i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
       let studentItem = `
@@ -46,15 +33,10 @@ function showPage(list, page) {
   }
 }
 
-
-
-
-
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Created the addPagination function which creates a button for every single page of data.
+It's also clickable which moves can move the pages forwards or backwards.
 */
-
 function addPagination(list) {
   var numOfPages = Math.ceil(list.length / 9);
   var linkList = document.querySelector('.link-list');
@@ -71,6 +53,7 @@ function addPagination(list) {
   let buttonActive = document.querySelector('.link-list button'); 
   buttonActive.className = 'active';
 
+  //changes the page and button styling when clicked
   linkList.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
       const removeClass = document.querySelector('.active');
@@ -85,7 +68,7 @@ showPage(data, 1);
 addPagination(data);
 
 
-//Was able to place a search box in he header - but struggled with the functionality part of it. I was close but couldn't quite get there. I've since looked at how other studens have done it and it makes sense now. 
+//Was able to place a search box in the header - but struggled with the functionality part of it. I was close but couldn't quite get there. I've since looked at how other students have done it and it makes sense now. 
 let header = document.querySelector('.header');
 let search = `
   <label for="search" class="student-search">
